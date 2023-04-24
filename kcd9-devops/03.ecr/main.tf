b91f2e1b6647832d66caccef0fca1c17902ac3c3
tog09 <tog09@naver.com>
2023-04-24 23:00:22 +0900

    chore: changed ecr repositories

--- kcd9-devops/03.ecr/main.tf
+++ kcd9-devops/03.ecr/main.tf
@@ -1,5 +1,19 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "kcd-cicd-kdyoo-app-repo"
+resource "aws_ecr_repository" "hello" {
+  name                 = "kcd-cicd-kdyoo-hello-app-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Terraform   = "true"
+    Environment = "kcd-cicd-kdyoo"
+  }
+}
+
+resource "aws_ecr_repository" "hi" {
+  name                 = "kcd-cicd-kdyoo-hi-app-repo"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
--- kcd9-devops/03.ecr/outputs.tf
+++ kcd9-devops/03.ecr/outputs.tf
@@ -1,7 +1,15 @@
-output "ecr_arn" {
-  value = aws_ecr_repository.app.arn
+output "ecr_hello_arn" {
+  value = aws_ecr_repository.hello.arn
 }
 
-output "ecr_repo_url" {
-  value = aws_ecr_repository.app.repository_url
+output "ecr_hello_repo_url" {
+  value = aws_ecr_repository.hello.repository_url
+}
+
+output "ecr_hi_arn" {
+  value = aws_ecr_repository.hi.arn
+}
+
+output "ecr_hi_repo_url" {
+  value = aws_ecr_repository.hi.repository_url
 }
